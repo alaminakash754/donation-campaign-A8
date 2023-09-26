98a94cb87df42a2df4b94aacd25b51785e44e75a
alamin <alaminakash9754@gmail.com>
2023-09-27 00:42:09 +0600

    adding a statistics section, and I've calculated the donation amounts, displaying them in a pie chart

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,53 +1,33 @@
+import { useEffect, useState } from 'react';
+import Chart from 'react-apexcharts'
+const Statistics = () => {
+  const [allDonation, setAllDonation] = useState([]);
+  const [noDataFound, setNoDataFound] = useState('');
 
+  useEffect(() => {
+    const getItem = JSON.parse(localStorage.getItem('donation'));
+    if(getItem){
+      setAllDonation(getItem);
+    }
+    else{
+      setNoDataFound('No data found');
+    }
+  },[])
 
-import React, { PureComponent } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+  const totalDonation = 12;
+  const myPercentage = (allDonation.length / totalDonation) * 100;
+  const myRemaining = 100 - myPercentage;
 
-const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-];
-
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
+  return(
+    <div>
+        <div>
+          {
+            noDataFound ? <p className='h-[80vh] flex justify-center items-center text-2xl'>{noDataFound}</p> :
+            <Chart className='h-[80vh] flex justify-center items-center' type='pie' height={500} series={[myPercentage, myRemaining]} options={{ labels: ['Your Donation', 'Total Donation']}}></Chart>
+          }
+        </div>
+    </div>
   );
 };
 
-export default class Example extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/s/pie-chart-with-customized-label-dlhhj';
-
-  render() {
-    return (
-      <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={400}>
-          <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
-    );
-  }
-}
+export default Statistics;
